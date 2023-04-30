ac3f7996b2b763e57e147aaf1c9ff54e93321617
decvfox <10021067@atu.ie>
2023-04-30 18:35:24 +0100

    updates to plotting, variables_txt & readme

--- plotting.py
+++ plotting.py
@@ -17,11 +17,23 @@ iris = load_iris()
 df= pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                  columns= iris['feature_names'] + ['target'])
  
+# plot histogram
+
+fig, ((ax, ax1), (ax2, ax3)) = plt.subplots(ncols=2, nrows=2,  figsize=(10, 10))
+
+df.plot(kind = 'hist', y = 'sepal length (cm)', color='c', alpha = 0.5, label = 'Sepal Length', ax=ax)
+df.plot(kind = 'hist', y = 'sepal width (cm)', color='m', alpha = 0.5, label = 'Sepal Width', ax=ax1)
+df.plot(kind = 'hist', y = 'petal length (cm)', color='y', alpha = 0.5, label = 'Petal Length', ax=ax2)
+df.plot(kind = 'hist', y = 'petal width (cm)', color='k', alpha = 0.5, label = 'Petal Width', ax=ax3)
+
+plt.savefig('histogram.png')
+
+#split out species
 df1 = df[0:50]
 df2 = df[50:100]
 df3 = df[100:150]
 
-# plot histogram
+# plot histogram coloured by species
 
 fig, ((ax, ax1), (ax2, ax3)) = plt.subplots(ncols=2, nrows=2,  figsize=(10, 10))
 
@@ -38,7 +50,7 @@ df1.plot(kind = 'hist', y = 'petal width (cm)', color='c', alpha = 0.5, label = 
 df2.plot(kind = 'hist', y = 'petal width (cm)', color='m', alpha = 0.5, label = 'Petal Width Versicolor', ax=ax3)
 df3.plot(kind = 'hist', y = 'petal width (cm)', color='y', alpha = 0.5, label = 'Petal Width Virginica', ax=ax3)
 
-plt.savefig('histogram.png')
+plt.savefig('histogram_species.png')
 
 # plot scatter
 
--- variable_txt.py
+++ variable_txt.py
@@ -13,16 +13,21 @@ iris = load_iris()
 df= pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                  columns= iris['feature_names'] + ['target'])
 
-def mean(col):
-    mean = df[col].mean()
+#split out species
+df1 = df[0:50]
+df2 = df[50:100]
+df3 = df[100:150]
+
+def mean(data_frame, col):
+    mean = data_frame[col].mean()
     return mean
 
-def min(col):
-    min = df[col].min()
+def min(data_frame, col):
+    min = data_frame[col].min()
     return min
 
-def max(col):
-    max = df[col].max()
+def max(data_frame, col):
+    max = data_frame[col].max()
     return max
 
 
@@ -30,14 +35,32 @@ def max(col):
 
 FILENAME = "variables.txt"
 
-Sepal_length = (f'''Sepal Length:
-The maximum Sepal length is {max('sepal length (cm)')}, the minimum is {min('sepal length (cm)')} and the mean is {round(mean('sepal length (cm)'),3)}\n''')
-Sepal_width = (f'''\nSepal Width:
-The maximum Sepal Width is {max('sepal width (cm)')}, the minimum is {min('sepal width (cm)')} and the mean is {round(mean('sepal width (cm)'),3)}\n''')
+Sepal_length = (f'''Sepal Length:\n
+The maximum Sepal length is {max(df, 'sepal length (cm)')}, the minimum is {min(df, 'sepal length (cm)')} and the mean is {round(mean(df, 'sepal length (cm)'),3)}\n
+For Setosa the maximum Sepal length is {max(df1, 'sepal length (cm)')}, the minimum is {min(df1, 'sepal length (cm)')} and the mean is {round(mean(df1, 'sepal length (cm)'),3)}
+For Versicolor the maximum Sepal length is {max(df2, 'sepal length (cm)')}, the minimum is {min(df2, 'sepal length (cm)')} and the mean is {round(mean(df2, 'sepal length (cm)'),3)}
+For Virginica the maximum Sepal length is {max(df3, 'sepal length (cm)')}, the minimum is {min(df3, 'sepal length (cm)')} and the mean is {round(mean(df3, 'sepal length (cm)'),3)}
+''')
+Sepal_width = (f'''\nSepal Width:\n
+The maximum Sepal Width is {max(df, 'sepal width (cm)')}, the minimum is {min(df, 'sepal width (cm)')} and the mean is {round(mean(df, 'sepal width (cm)'),3)}\n
+For Setosa the maximum Sepal Width is {max(df1, 'sepal width (cm)')}, the minimum is {min(df1, 'sepal width (cm)')} and the mean is {round(mean(df1, 'sepal width (cm)'),3)}
+For Versicolor the maximum Sepal Width is {max(df2, 'sepal width (cm)')}, the minimum is {min(df2, 'sepal width (cm)')} and the mean is {round(mean(df2, 'sepal width (cm)'),3)}
+For Virginica the maximum Sepal Width is {max(df3, 'sepal width (cm)')}, the minimum is {min(df3, 'sepal width (cm)')} and the mean is {round(mean(df3, 'sepal width (cm)'),3)}
+''')
+
 Petal_length = (f'''\nPetal Length:
-The maximum Petal length is {max('petal length (cm)')}, the minimum is {min('petal length (cm)')} and the mean is {round(mean('petal length (cm)'),3)}\n''')
+The maximum Petal length is {max(df, 'petal length (cm)')}, the minimum is {min(df, 'petal length (cm)')} and the mean is {round(mean(df, 'petal length (cm)'),3)}\n
+For Setosa the maximum Petal length is {max(df1, 'petal length (cm)')}, the minimum is {min(df1, 'petal length (cm)')} and the mean is {round(mean(df1, 'petal length (cm)'),3)}
+For Versicolor the maximum Petal length is {max(df2, 'petal length (cm)')}, the minimum is {min(df2, 'petal length (cm)')} and the mean is {round(mean(df2, 'petal length (cm)'),3)} 
+For Virginica the maximum Petal length is {max(df3, 'petal length (cm)')}, the minimum is {min(df3, 'petal length (cm)')} and the mean is {round(mean(df3, 'petal length (cm)'),3)}
+''')
 Petal_width	= (f'''\nPetal Width:
-The maximum Petal Width is {max('petal width (cm)')}, the minimum is {min('petal width (cm)')} and the mean is {round(mean('petal width (cm)'),3)}\n''')
+The maximum Petal Width is {max(df, 'petal width (cm)')}, the minimum is {min(df, 'petal width (cm)')} and the mean is {round(mean(df, 'petal width (cm)'),3)}\n
+For Setosa the maximum Petal Width is {max(df1, 'petal width (cm)')}, the minimum is {min(df1, 'petal width (cm)')} and the mean is {round(mean(df1, 'petal width (cm)'),3)}
+For Versicolor the maximum Petal Width is {max(df2, 'petal width (cm)')}, the minimum is {min(df2, 'petal width (cm)')} and the mean is {round(mean(df2, 'petal width (cm)'),3)}
+For Virginica the maximum Petal Width is {max(df3, 'petal width (cm)')}, the minimum is {min(df3, 'petal width (cm)')} and the mean is {round(mean(df3, 'petal width (cm)'),3)}
+''')
+
 Species = '''\nSpecies:
 This will hold a summary of what the species variable is and the data it contains\n'''
 
